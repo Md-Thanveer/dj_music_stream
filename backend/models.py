6e34f89043aa98ea2665f1be3765542dbc9c7864
Md-Thanveer <thanveerami612@gmail.com>
2025-02-26 19:21:47 +0530

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,32 @@
 from django.db import models
 
-# Create your models here.
 class Genre(models.Model):
-    name=models.CharField(max_length=100,unique=True)
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name= 'Genre'
+        verbose_name = 'Genre'
         verbose_name_plural = 'Genres'
+
+class Company(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    company_name = models.CharField(max_length=100, unique=True)
+    image_path = models.ImageField(upload_to='media/company', null=True, blank=True, default='no_image_available.png')
+
+    def __str__(self):
+        return self.company_name
+
+
+class Song(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, unique=True)
+    image_path = models.ImageField(upload_to='media/song_cover', null=True, blank=True, default='no_image_available.png')
+    song_path = models.ImageField(upload_to='media/song', null=True, blank=True, default='no_image_available.png')
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
