258e0a97188440336ceecd95ad61ef462876d04b
Md-Thanveer <thanveerami612@gmail.com>
2025-02-26 19:22:07 +0530

    Update admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from backend.models import Genre
+from backend.models import Genre, Company, Song
 
 # Register your models here.
 admin.site.register(Genre)
+admin.site.register(Company)
+
+admin.site.register(Song)
