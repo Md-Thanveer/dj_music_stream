ff250966fceb91f6c67d0dfe5e113cee0f04b9c5
Md-Thanveer <thanveerami612@gmail.com>
2025-02-27 12:19:19 +0530

    no_image_available added in admin.py

--- backend/admin.py
+++ backend/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from backend.models import Genre, Company, Song
 
 # Register your models here.
 admin.site.register(Genre)
-admin.site.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'company_name','image_tag')
+
+    def image_tag(self,obj):
+        return format_html('<img src="{}" width="150" height="150"'.format(obj.image_path.url))
+admin.site.register(Company,CompanyAdmin)
 
 admin.site.register(Song)
